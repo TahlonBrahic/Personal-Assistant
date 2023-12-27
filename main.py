ee74ac9da902d7d0c2ab59cfa50f0fbb23d146f8
Tahlon Brahic <104690672+TahlonBrahic@users.noreply.github.com>
2023-12-27 14:38:49 -0600

    Import whisper speech to text model

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import os
 import pathlib
 import platform
+import whisper
 import speech_recognition
+
+# Load model
+whisper = whisper.load_model('medium.en')
 
 speech = speech_recognition.Recognizer()
 print('Listening')
@@ -9,5 +13,5 @@ print('Listening')
 with speech_recognition.Microphone() as source:
     speech.adjust_for_ambient_noise(source)
     audio = speech.listen(source)
-    input = speech.recognize_sphinx(audio)
+    input = speech.recognize_whisper(audio, model=whisper)
     print(f'You just said {input}')
