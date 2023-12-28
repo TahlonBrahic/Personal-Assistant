0b7893992e9b58b96a3650f9b0eaecfe6092c0da
Tahlon Brahic <104690672+TahlonBrahic@users.noreply.github.com>
2023-12-28 09:42:50 -0600

    Turn the sphinx model into a listening function

--- main.py
+++ main.py
@@ -13,17 +13,21 @@ import speech_recognition as sr
 # LIB ENVIRONMENT VARIABLE: 'C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\14.38.33130\lib\x64'
 # PATH ENVIRONMENT VARIABLE: 'C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\14.38.33130\bin\Hostx64\x64'
 
-speech = sr.Recognizer()
-print('Listening')
+# This function listens to one audio input with the sphinx model. 
+def listen_with_sphinx():
+    speech = sr.Recognizer()
+    print('Listening')
 
-with sr.Microphone() as source:
-    speech.adjust_for_ambient_noise(source)
-    audio = speech.listen(source)
+    with sr.Microphone() as source:
+        speech.adjust_for_ambient_noise(source)
+        audio = speech.listen(source)
 
-    try:
-        spoken_text = speech.recognize_sphinx(audio)
-        print(f'You just said: {spoken_text}')
-    except sr.UnknownValueError:
-        print("Sorry, I did not understand that.")
-    except sr.RequestError as e:
-        print(f"Could not request results; {e}")
+        try:
+            spoken_text = speech.recognize_sphinx(audio)
+            print(f'You just said: {spoken_text}')
+        except sr.UnknownValueError:
+            print("Sorry, I did not understand that.")
+        except sr.RequestError as e:
+            print(f"Could not request results; {e}")
+
+listen_with_sphinx()
