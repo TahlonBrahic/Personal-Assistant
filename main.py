2aedb1b9415bcdac928d8495f5459ac390a70a88
Tahlon Brahic <104690672+TahlonBrahic@users.noreply.github.com>
2023-12-27 22:55:08 -0600

    Create error handling

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import os
 import pathlib
 import platform
-import speech_recognition
+import speech_recognition as sr
 
 # local path 'C:\Users\tahlo\Documents\Programming\Personal-Assistant'
 # virtual env path 'C:\Users\tahlo\Documents\Programming\Personal-Assistant\virtual-assistant-virtualenv\Scripts'
 # venv interpreter C:\Users\tahlo\Documents\Programming\Personal-Assistant\pavenv\Scripts\python.exe
-
-speech = speech_recognition.Recognizer()
+speech = sr.Recognizer()
 print('Listening')
 
-with speech_recognition.Microphone() as source:
+with sr.Microphone() as source:
     speech.adjust_for_ambient_noise(source)
     audio = speech.listen(source)
-    input = speech.recognize_sphinx(audio)
-    print(f'You just said {input}')
+
+    try:
+        spoken_text = speech.recognize_sphinx(audio)
+        print(f'You just said: {spoken_text}')
+    except sr.UnknownValueError:
+        print("Sorry, I did not understand that.")
+    except sr.RequestError as e:
+        print(f"Could not request results; {e}")
