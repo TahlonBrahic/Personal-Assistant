d525035d40aedb1a4450cf27a07777a1e6e59894
Tahlon Brahic <104690672+TahlonBrahic@users.noreply.github.com>
2023-12-28 09:27:44 -0600

    Include environment variables

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import os
 import pathlib
 import platform
+import whisper
 import speech_recognition as sr
 
 # local path 'C:\Users\tahlo\Documents\Programming\Personal-Assistant'
 # virtual env path 'C:\Users\tahlo\Documents\Programming\Personal-Assistant\virtual-assistant-virtualenv\Scripts'
 # venv interpreter C:\Users\tahlo\Documents\Programming\Personal-Assistant\pavenv\Scripts\python.exe
+
+# INCLUDE ENVIRONMENT VARIABLE: 'C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\14.38.33130\include'
+# LIB ENVIRONMENT VARIABLE: 'C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\14.38.33130\lib\x64'
+# PATH ENVIRONMENT VARIABLE: 'C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\14.38.33130\bin\Hostx64\x64'
+
 speech = sr.Recognizer()
 print('Listening')
 
