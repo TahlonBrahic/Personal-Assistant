99e87fb5e8e6285821b63d6024bc1ba211332b32
Tahlon Brahic <104690672+TahlonBrahic@users.noreply.github.com>
2024-01-05 14:15:47 -0600

    Move label position and resize

--- gui.py
+++ gui.py
@@ -3,12 +3,12 @@ from PyQt5.QtWidgets import QLabel, QMainWindow
 class GUI(QMainWindow):
     def __init__(self, label_text='Listening...', height=400):
         super().__init__()
-        self.label = QLabel(label_text)
         self.setWindowTitle("Voice Assistant")
         self.setFixedHeight(height)
+        self.label = QLabel(label_text)
+        self.label.resize(120, 80)
 
     def display(self):
-        self.label.show()
         self.show()
 
     def update_text(self, text):
