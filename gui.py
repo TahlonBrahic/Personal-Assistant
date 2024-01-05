b23692a026c0a6d9633d2a470262f17021b6c159
Tahlon Brahic <104690672+TahlonBrahic@users.noreply.github.com>
2024-01-05 17:02:57 -0600

    Remove method and change class variable

--- gui.py
+++ gui.py
@@ -4,7 +4,7 @@ from PyQt5.QtCore import Qt
 class GUI(QMainWindow):
     def __init__(self, label_text='Listening...', height=400):
         super().__init__()
-        self.label = label_text
+        self.label_text = label_text
 
         self.setWindowTitle("Voice Assistant")
         self.setGeometry(100, 100, 800, 600)
@@ -21,7 +21,7 @@ class GUI(QMainWindow):
 
         # Create a Layout and Label, centers the label
         layout = QVBoxLayout()
-        label = QLabel(self.label, centralWidget)
+        label = QLabel(self.label_text, centralWidget)
         label.setAlignment(Qt.AlignCenter)
 
         # Add widget to label and set layout of central widget
@@ -33,10 +33,6 @@ class GUI(QMainWindow):
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
-
-    def inputSpeechWidget(self):
-        self.label = QLabel(self)
-        self.label.resize(120, 80)
 
     def display(self):
         self.show()
