65f315fda01ace961524ee2f1a9b77b46e769bc8
Tahlon Brahic <104690672+TahlonBrahic@users.noreply.github.com>
2024-01-05 16:52:23 -0600

    Add the createCentralWidget method

--- gui.py
+++ gui.py
@@ -1,13 +1,41 @@
-from PyQt5.QtWidgets import QLabel, QMainWindow
+from PyQt5.QtWidgets import QLabel, QMainWindow, QDesktopWidget, QWidget, QVBoxLayout
+from PyQt5.QtCore import Qt
 
 class GUI(QMainWindow):
     def __init__(self, label_text='Listening...', height=400):
         super().__init__()
-        self.setWindowTitle("Voice Assistant")
-        self.setFixedHeight(height)
+        self.label_text = label_text
 
-        # Label information
-        self.label = QLabel(label_text)
+        self.setWindowTitle("Voice Assistant")
+        self.setGeometry(100, 100, 800, 600)
+        self.centerWindow()
+
+        self.createCentralWidget()
+
+    # This creates the central widget and adds the label that reads the input text.
+    def createCentralWidget(self):
+
+        # Create the central widget
+        centralWidget = QWidget(self)
+        self.setCentralWidget(centralWidget)
+
+        # Create a Layout and Label, centers the label
+        layout = QVBoxLayout()
+        label = QLabel(self.label_text, centralWidget)
+        label.setAlignment(Qt.AlignCenter)
+
+        # Add widget to label and set layout of central widget
+        layout.addWidget(label)
+        centralWidget.setLayout(layout)
+
+    def centerWindow(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def inputSpeechWidget(self):
+        self.label = QLabel(self.label_text)
         self.label.resize(120, 80)
 
     def display(self):
