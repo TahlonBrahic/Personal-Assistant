5eea9b082847f329215b6177f98f1c4cb21411fc
Tahlon Brahic <104690672+TahlonBrahic@users.noreply.github.com>
2024-01-05 12:56:47 -0600

    Set fixed height

--- gui.py
+++ gui.py
@@ -1,10 +1,11 @@
 from PyQt5.QtWidgets import QLabel, QMainWindow
 
 class GUI(QMainWindow):
-    def __init__(self, label_text='Listening...'):
+    def __init__(self, label_text='Listening...', height=400):
         super().__init__()
         self.label = QLabel(label_text)
         self.setWindowTitle("Voice Assistant")
+        self.setFixedHeight(height)
 
     def display(self):
         self.show()
