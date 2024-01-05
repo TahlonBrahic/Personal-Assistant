84360a859cd20daef1434c36cb486301862bde3f
Tahlon Brahic <104690672+TahlonBrahic@users.noreply.github.com>
2024-01-04 18:42:07 -0600

    Turn gui into OOP format

--- gui.py
+++ gui.py
@@ -1,8 +1,14 @@
 from PyQt5.QtWidgets import QApplication, QLabel
 
-def display():
-    app = QApplication([])
-    label = QLabel('Listening...')
-    label.show()
-    app.exec()
+class GUI:
+    def __init__(self):
+        self.app = QApplication([])
+        self.label = QLabel('Listening...')
+
+    def display(self):
+        self.label.show()
+        self.app.exec()
+
+    def update_text(self, text):
+        self.label.setText(text)
 
