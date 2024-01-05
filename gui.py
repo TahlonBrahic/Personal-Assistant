f578c4f3bf9fe73e43833dc6669a561ebdb9416b
Tahlon Brahic <104690672+TahlonBrahic@users.noreply.github.com>
2024-01-04 19:08:14 -0600

    Fix gui: remove calls that block the event loop that stop multithreading

--- gui.py
+++ gui.py
@@ -2,13 +2,11 @@ from PyQt5.QtWidgets import QApplication, QLabel
 
 class GUI:
     def __init__(self, label_text='Listening...'):
-        self.app = QApplication([])
         self.label = QLabel(label_text)
 
     def display(self):
         self.label.show()
-        self.app.exec()
+
 
     def update_text(self, text):
-        self.label = QLabel(text)
-
+         self.label.setText(text)  # Update text of the existing label
