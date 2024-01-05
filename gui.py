e744be28f8cf9c027a4f8841d8d902075edd20ab
Tahlon Brahic <104690672+TahlonBrahic@users.noreply.github.com>
2024-01-05 12:21:18 -0600

    Set window title

--- gui.py
+++ gui.py
@@ -3,10 +3,10 @@ from PyQt5.QtWidgets import QApplication, QLabel
 class GUI:
     def __init__(self, label_text='Listening...'):
         self.label = QLabel(label_text)
+        self.setWindowTitle("Personal Assistant")
 
     def display(self):
         self.label.show()
-
 
     def update_text(self, text):
          self.label.setText(text)  # Update text of the existing label
