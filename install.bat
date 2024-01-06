7a96cdcc428b7cb38f01f92d55a6ba35cc60c70c
Tahlon Brahic <104690672+TahlonBrahic@users.noreply.github.com>
2024-01-05 20:41:02 -0600

    Add exit commands

--- install.bat
+++ install.bat
@@ -1,2 +1,18 @@
 @echo off
-mkdir "%LOCALAPPDATA%\TellTruVirtualAssistant"
+
+echo Creating application directory...
+mkdir "%LOCALAPPDATA%\TellTruVirtualAssistant" || exit /b
+
+
+echo Setting up Python virtual environment...
+python -m venv "%LOCALAPPDATA%\TellTruVirtualAssistant\venv" || exit /b
+
+echo Copying necessary files...
+python -m venv "%LOCALAPPDATA%\TellTruVirtualAssistant\venv" || exit /b
+
+echo Installing dependencies...
+"%LOCALAPPDATA%\TellTruVirtualAssistant\venv\Scripts\pip.exe" install -r requirements.txt || exit /b
+
+echo Setup completed successfully.
+
+pause
