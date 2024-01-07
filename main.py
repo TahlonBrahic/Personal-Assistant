2539925b6d3e7d77aaa7ecd6f76f0c2b4a4cb616
Tahlon Brahic <104690672+TahlonBrahic@users.noreply.github.com>
2024-01-07 15:13:16 -0600

    Create personal_assistant.py and modify create placement of imports

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import os
 import sys
-import time
-import pathlib
 
 import speech_recognition as sr
 from PyQt5.QtWidgets import QApplication
@@ -10,24 +7,8 @@ from PyQt5.QtWidgets import QApplication
 
 # Local file imports
 sys.path.append('C:/Users/tahlo/Documents/Programming/Personal-Assistant')
-import lists
-import gui
 from speech_recognizer import SpeechRecognizer
-
-class PersonalAssistant:
-    def __init__(self, model='whisper'):
-        self.recognizer = SpeechRecognizer(model=model)
-        self.gui = gui.GUI()
-        self.recognizer.recognized_signal.connect(self.on_recognized)
-
-    def on_recognized(self, text):
-        # Update GUI with recognized text
-        self.gui.update_text(text)  # Ensure GUI has a method to update text
-
-    def start(self):
-        self.gui.display()
-        self.recognizer.start()
-        
+from personal_assistant import PersonalAssitant
 
 if __name__ == '__main__':
     app = QApplication(sys.argv) # Create instance of a QApplication
---
+++ personal_assistant.py
@@ -0,0 +1,15 @@
+from gui import GUI
+
+class PersonalAssistant:
+    def __init__(self, model='whisper'):
+        self.recognizer = SpeechRecognizer(model=model)
+        self.gui = GUI()
+        self.recognizer.recognized_signal.connect(self.on_recognized)
+
+    def on_recognized(self, text):
+        # Update GUI with recognized text
+        self.gui.update_text(text)  # Ensure GUI has a method to update text
+
+    def start(self):
+        self.gui.display()
+        self.recognizer.start()
