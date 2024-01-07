c17bec4ff5ab3aeafd62f85229b6d1c22c4025a1
Tahlon Brahic <104690672+TahlonBrahic@users.noreply.github.com>
2024-01-07 15:09:02 -0600

    Create config.py

--- main.py
+++ main.py
@@ -14,17 +14,6 @@ import lists
 import gui
 from speech_recognizer import SpeechRecognizer
 
-# The following section provides information for me on important locations to run the application from source code.
-
-# local path 'C:\Users\tahlo\Documents\Programming\Personal-Assistant'
-# virtual env path 'C:\Users\tahlo\Documents\Programming\Personal-Assistant\pavenv\Scripts'
-# start virtual environemnt: source activate
-# venv interpreter C:\Users\tahlo\Documents\Programming\Personal-Assistant\pavenv\Scripts\python.exe
-
-# INCLUDE ENVIRONMENT VARIABLE: 'C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\14.38.33130\include'
-# LIB ENVIRONMENT VARIABLE: 'C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\14.38.33130\lib\x64'
-# PATH ENVIRONMENT VARIABLE: 'C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\14.38.33130\bin\Hostx64\x64'
-
 class PersonalAssistant:
     def __init__(self, model='whisper'):
         self.recognizer = SpeechRecognizer(model=model)
