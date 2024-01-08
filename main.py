cb3b095db873607efcaaab7e2dff0d78c54c479f
Tahlon Brahic <104690672+TahlonBrahic@users.noreply.github.com>
2024-01-07 22:44:33 -0600

    Add system path change

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from personal_assistant import PersonalAssistant
 from config import SystemPath
+from personal_assistant import PersonalAssistant
 
 def main():
     """Main function to start the voice assistant application."""
     try:
-        system_path = SystemPath() # Create instance of SystemPath
-        system_path.append() # Append system path so Python can find local modules   
+        SystemPath().append()
         app = QApplication(sys.argv)  # Create instance of a QApplication
         assistant = PersonalAssistant()  # Create instance of a PersonalAssistant
         assistant.start()  # Start the voice assistant
