b45bbac7eec6a939f5fb9513cf40374489449b21
Tahlon Brahic <104690672+TahlonBrahic@users.noreply.github.com>
2024-01-07 21:48:55 -0600

    Correct import

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import QApplication
 
 # Local file imports
 sys.path.append('C:/Users/tahlo/Documents/Programming/Personal-Assistant')
-from personal_assistant import PersonalAssitant
+from personal_assistant import PersonalAssistant
 
 if __name__ == '__main__':
     app = QApplication(sys.argv) # Create instance of a QApplication
